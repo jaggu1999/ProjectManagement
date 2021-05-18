e418094cb3817a183c3e388d068a08ee7974526e
Ch Jagadeesh <ch.jagadeeshkumarc@gmail.com>
2021-05-19 02:07:44 +0530

    Added Intro, Login and Signup Pages

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-import './App.css';
+import "./App.css";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "./Styles/NavBar.css";
+import "./Fonts/fonts.css";
+import "./Pages/IntroScreen"
+import IntroScreen from "./Pages/IntroScreen";
+import Login from "./Pages/Login";
+import Signup from "./Pages/Signup";
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    //<IntroScreen></IntroScreen>
+    //<Login></Login>
+    <Signup></Signup>
   );
 }
 
---
+++ src/Components/NavBar.js
@@ -0,0 +1,34 @@
+import React from "react";
+import { FaBars } from "react-icons/fa";
+import "../SVGs/LogoLight"
+import Logo from "../SVGs/LogoLight";
+import GetStarted from "../SVGs/GetStarted";
+
+const NavBar = () => {
+    return (
+        <nav className="navbar navbar-expand-lg navbar-light" style={{backgroundColor:"#4a2849"}}>
+            <div className="container-fluid">
+                <a className="navbar-brand" href="">
+                    <Logo></Logo>
+                </a>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <FaBars color="#f0e5f6" size={30}/>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul className="navbar-nav ms-auto">
+                        <li className="nav-item">
+                            <a className="nav-link active" aria-current="page" href="#">Login</a>
+                        </li>
+                        <li className="nav-item">
+                        <a className="nav-link" href="#">
+                            <GetStarted></GetStarted>
+                        </a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    )
+}
+
+export default NavBar
